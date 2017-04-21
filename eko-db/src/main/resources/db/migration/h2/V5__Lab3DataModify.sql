9ef99fff2d18b67be7867899f068fcad8362a449
Andrey Derevyanko <777alterego777@gmail.com>
2017-04-21 23:13:48 +0300

    Update V5__Lab3DataModify.sql

--- eko-db/src/main/resources/db/migration/h2/V5__Lab3DataModify.sql
+++ eko-db/src/main/resources/db/migration/h2/V5__Lab3DataModify.sql
@@ -1,3 +1,3 @@
 ALTER TABLE LAB3DATA ADD completed BOOLEAN NOT NULL DEFAULT FALSE;
 CREATE UNIQUE INDEX ix_lab3data_user_id_start_date ON LAB3DATA(USER_ID,START_DATE);
-COMMENT ON COLUMN LAB3DATA.COMPLETED IS 'Параметр завершенности лабораторной работы'
+COMMENT ON COLUMN LAB3DATA.COMPLETED IS 'Признак завершенности лабораторной работы';
