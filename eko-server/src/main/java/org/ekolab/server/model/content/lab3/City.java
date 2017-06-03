fb5c140855a2e93f8ed494ac83c021c129c72bbb
alekseevnp <alekseevnp@list.ru>
2017-06-03 18:21:14 +0300

    Для City установлено соответствие с типом топлива для конкретного города

--- eko-server/src/main/java/org/ekolab/server/model/content/lab3/City.java
+++ eko-server/src/main/java/org/ekolab/server/model/content/lab3/City.java
@@ -1,17 +1,98 @@
 package org.ekolab.server.model.content.lab3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 777Al on 11.04.2017.
  */
 public enum City {
-    MOSCOW,
-    SAINT_PETERSBURG,
-    NOVOSIBIRSK,
-    EKATERINBURG,
-    NIZHNIY_NOVGOROD,
-    KRASNOYARSK,
-    ROSTOV_NA_DONU,
-    KALININGRAD,
-    VLADIVOSTOK,
-    SURGUT
+    MOSCOW{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.DONETSK_COAL);
+            FuelTypesForTheCity.add(FuelType.SULFUR_OIL);
+            return FuelTypesForTheCity;
+        }
+    },
+    SAINT_PETERSBURG{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.SULFUR_OIL);
+            FuelTypesForTheCity.add(FuelType.LENINGRAD_SHALE);
+            FuelTypesForTheCity.add(FuelType.ESTONIAN_SHALE);
+            return FuelTypesForTheCity;
+        }
+    },
+    NOVOSIBIRSK{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.EKIBASTUZ_COAL);
+            FuelTypesForTheCity.add(FuelType.BEREZOVSKY_COAL_TSU);
+            FuelTypesForTheCity.add(FuelType.BEREZOVSKY_COAL_ZHSHU);
+            FuelTypesForTheCity.add(FuelType.KUZNETSK_COAL);
+            return FuelTypesForTheCity;
+        }
+    },
+    EKATERINBURG{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.EKIBASTUZ_COAL);
+            FuelTypesForTheCity.add(FuelType.SULFUR_OIL);
+            FuelTypesForTheCity.add(FuelType.KUZNETSK_COAL);
+            return FuelTypesForTheCity;
+        }
+    },
+    NIZHNIY_NOVGOROD{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.DONETSK_COAL);
+            FuelTypesForTheCity.add(FuelType.SULFUR_OIL);
+            return FuelTypesForTheCity;
+        }
+    },
+    KRASNOYARSK{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.EKIBASTUZ_COAL);
+            FuelTypesForTheCity.add(FuelType.BEREZOVSKY_COAL_TSU);
+            FuelTypesForTheCity.add(FuelType.BEREZOVSKY_COAL_ZHSHU);
+            FuelTypesForTheCity.add(FuelType.KUZNETSK_COAL);
+            return FuelTypesForTheCity;
+        }
+    },
+    ROSTOV_NA_DONU{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.DONETSK_COAL);
+            FuelTypesForTheCity.add(FuelType.SULFUR_OIL);
+            return FuelTypesForTheCity;
+        }
+    },
+    KALININGRAD{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.DONETSK_COAL);
+            FuelTypesForTheCity.add(FuelType.SULFUR_OIL);
+            return FuelTypesForTheCity;
+        }
+    },
+    VLADIVOSTOK{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.EKIBASTUZ_COAL);
+            FuelTypesForTheCity.add(FuelType.BEREZOVSKY_COAL_TSU);
+            FuelTypesForTheCity.add(FuelType.BEREZOVSKY_COAL_ZHSHU);
+            FuelTypesForTheCity.add(FuelType.KUZNETSK_COAL);
+            return FuelTypesForTheCity;
+        }
+    },
+    SURGUT{
+        public List<FuelType> getFuelTypesForTheCity(){
+            ArrayList<FuelType> FuelTypesForTheCity = new ArrayList<>();
+            FuelTypesForTheCity.add(FuelType.SULFUR_OIL);
+            FuelTypesForTheCity.add(FuelType.STABILIZED_OIL);
+            return FuelTypesForTheCity;
+        }
+    }
 }
--- eko-server/src/main/java/org/ekolab/server/model/content/lab3/FuelType.java
+++ eko-server/src/main/java/org/ekolab/server/model/content/lab3/FuelType.java
@@ -4,5 +4,14 @@ package org.ekolab.server.model.content.lab3;
  * Created by 777Al on 11.04.2017.
  */
 public enum FuelType {
-    SULFUR_OIL
+    DONETSK_COAL,
+    EKIBASTUZ_COAL,
+    KUZNETSK_COAL,
+    BEREZOVSKY_COAL_TSU,
+    BEREZOVSKY_COAL_ZHSHU,
+    ESTONIAN_SHALE,
+    LENINGRAD_SHALE,
+    SULFUR_OIL,
+    STABILIZED_OIL
 }
+
