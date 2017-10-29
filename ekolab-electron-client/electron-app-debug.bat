94364e504efda6260b6794e9f12f5befc2dd8de1
a-derevyanko <13*06**93>
2017-10-29 13:54:28 +0300

    Удаление клиента на electron из master ветки

--- ekolab-electron-client/electron-app-debug.bat
+++
@@ -1,6 +0,0 @@
-rmdir .\electron-src\electron-vaadin\ /s /q
-xcopy .\build\install\electron-vaadin .\electron-src\electron-vaadin\ /E
-xcopy .\build\resources\main\app.properties .\electron-src\ /E /Y
-cd electron-src
-call .\node_modules\.bin\electron .
-cd ..
