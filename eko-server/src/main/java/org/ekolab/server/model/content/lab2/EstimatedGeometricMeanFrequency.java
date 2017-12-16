79e201ba5273ac574543e7acf85accde6d5c540c
Андрей Деревянко <AVDerevyanko.SBT@sberbank.ru>
2017-12-16 13:43:17 +0300

    Добавлены файлы для презентации

---
+++ eko-server/src/main/java/org/ekolab/server/model/content/lab2/EstimatedGeometricMeanFrequency.java
@@ -0,0 +1,13 @@
+package org.ekolab.server.model.content.lab2;
+
+public enum EstimatedGeometricMeanFrequency {
+    F_315,
+    F_63,
+    F_125,
+    F_250,
+    F_500,
+    F_1000,
+    F_2000,
+    F_4000,
+    F_8000
+}
