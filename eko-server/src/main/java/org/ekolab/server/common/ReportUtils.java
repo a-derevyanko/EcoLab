43c60b3b7091b1103e42ce73ce8c260840c5e68f
Деревянко Андрей Владимирович <AVDerevyanko.SBT@sberbank.ru>
2018-01-13 08:34:42 +0300

    Отчёт для второй лабораторной

--- eko-server/src/main/java/org/ekolab/server/common/ReportUtils.java
+++ eko-server/src/main/java/org/ekolab/server/common/ReportUtils.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.SortedMap;
 
 public class ReportUtils {
-    public static <T, V> String getFromMap(SortedMap<T, List<V>> map, T key, int index, String defaultValue) {
-        return map.containsKey(key) ? String.valueOf(map.get(key).get(index)) : defaultValue;
+    public static <T, V> String getFromMap(SortedMap<T, List<V>> map, T key, int index) {
+        return map.containsKey(key) ? String.valueOf(map.get(key).get(index)) : "";
     }
 }
