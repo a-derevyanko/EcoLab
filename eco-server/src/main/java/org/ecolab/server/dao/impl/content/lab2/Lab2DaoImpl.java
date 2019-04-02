b52d1869b2627386d0c410a5ea8bdaaa2214f523
Деревянко Андрей Владимирович <Derevyanko.A.Vl@sberbank.ru>
2019-04-02 08:43:33 +0300

    Исправлен дефект с  незаполненным массивом

--- eco-server/src/main/java/org/ecolab/server/dao/impl/content/lab2/Lab2DaoImpl.java
+++ eco-server/src/main/java/org/ecolab/server/dao/impl/content/lab2/Lab2DaoImpl.java
@@ -164,7 +164,10 @@ public abstract class Lab2DaoImpl<V extends Lab2Variant> extends LabDaoImpl<Lab2
     }
 
     protected static List<Double> toDoubleList(Object[] array) {
-        return array == null ? null : Arrays.stream(array).map(o -> (Double) o).collect(Collectors.toList());
+        return array == null ? null : Arrays.stream(array).map(o -> o instanceof Number ?
+                Double.valueOf(((Number) o).doubleValue()) :
+                Double.valueOf(String.valueOf(o))
+        ).collect(Collectors.toList());
     }
 
     protected abstract Lab2DataMapper<V> getLabMapper();
